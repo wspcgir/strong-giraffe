204dc3274d8614e56c52dae449455f40388ce78a
JD95 <jeffreydwyer95@outlook.com>
2026-06-20 12:26:22 -0700

    Cleans up plot code

--- app/src/main/java/org/wspcgir/strong_giraffe/views/plot/BarPlot.kt
+++ app/src/main/java/org/wspcgir/strong_giraffe/views/plot/BarPlot.kt
@@ -1,14 +1,13 @@
 package org.wspcgir.strong_giraffe.views.plot
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -23,7 +22,10 @@ import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottom
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberColumnCartesianLayer
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
+import com.patrykandpatrick.vico.compose.common.ProvideVicoTheme
 import com.patrykandpatrick.vico.compose.common.component.rememberLineComponent
+import com.patrykandpatrick.vico.compose.m3.common.rememberM3VicoTheme
+import com.patrykandpatrick.vico.core.cartesian.axis.Axis
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
@@ -33,114 +35,145 @@ import com.patrykandpatrick.vico.core.cartesian.layer.ColumnCartesianLayer
 import com.patrykandpatrick.vico.core.common.Fill
 import com.patrykandpatrick.vico.core.common.component.LineComponent
 import com.patrykandpatrick.vico.core.common.data.ExtraStore
+import org.wspcgir.strong_giraffe.ui.theme.StrongGiraffeTheme
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.collections.listOf
-import kotlin.random.Random
 
-data class SetRecord(
-    val dateTime: String,
-    val weight: Float,
+data class SeriesValue(
+    val label: String,
+    val height: Float,
     val fill: Fill
 )
 
 var difficultyColorsKey = ExtraStore.Key<List<Fill>>()
 
 @Composable
-fun BarPlot(workoutSets: List<SetRecord>) {
+fun BarPlot(workoutSets: List<SeriesValue>, modifier: Modifier = Modifier) {
     val modelProducer = remember { CartesianChartModelProducer() }
     var isChartLoaded by remember { mutableStateOf(false) }
     LaunchedEffect(workoutSets) {
-        modelProducer.runTransaction {
-            columnSeries{ series(workoutSets.map { it.weight }) }
-            extras { store ->
-                store[difficultyColorsKey] = workoutSets.map { it.fill }
-            }
-        }
+        setupCharSeries(modelProducer, workoutSets)
         isChartLoaded = true
     }
-    key(isChartLoaded) {
-        Box {
-            if (isChartLoaded) {
-                val baseLineComponent = rememberLineComponent(
-                    strokeThickness = 0.dp,
-                    strokeFill = Fill(Color.Black.toArgb())
-                )
-                CartesianChartHost(
-                    modifier = Modifier.fillMaxHeight(0.5f).fillMaxWidth(),
-                    modelProducer = modelProducer,
-                    chart = rememberCartesianChart(
-                        rememberColumnCartesianLayer(
-                            columnProvider = remember(baseLineComponent) {
-                                object : ColumnCartesianLayer.ColumnProvider {
-                                    override fun getColumn(
-                                        entry: ColumnCartesianLayerModel.Entry,
-                                        seriesIndex: Int,
-                                        extraStore: ExtraStore
-                                    ): LineComponent {
-                                        val colors = extraStore[difficultyColorsKey]
-                                        val targetColor = colors.getOrNull(entry.x.toInt()) ?: Fill(Color.Black.toArgb())
-                                        return baseLineComponent.copy(
-                                            fill = targetColor,
-                                        )
-                                    }
-
-                                    override fun getWidestSeriesColumn(
-                                        seriesIndex: Int,
-                                        extraStore: ExtraStore
-                                    ): LineComponent {
-                                        return baseLineComponent
-                                    }
-                                }
-                            },
-                            columnCollectionSpacing = 0.dp,
-                        ),
-                        startAxis = VerticalAxis.rememberStart(),
-                        bottomAxis = HorizontalAxis.rememberBottom(
-                            valueFormatter = { _, value, _ ->
-                                workoutSets.getOrNull(value.toInt())?.dateTime ?: "-"
-                            }
-                        )
-                    ),
-                )
-            } else {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text("Loading plot...")
-                }
-            }
-
+    Box {
+        if (isChartLoaded) {
+            Chart(modelProducer, workoutSets, modifier)
+        } else {
+            ChartLoading(modifier)
         }
     }
 }
 
-@Preview(showBackground = true)
+private suspend fun setupCharSeries(
+    modelProducer: CartesianChartModelProducer,
+    workoutSets: List<SeriesValue>,
+): Unit {
+    modelProducer.runTransaction {
+        columnSeries { series(workoutSets.map { it.height }) }
+        extras { store ->
+            store[difficultyColorsKey] = workoutSets.map { it.fill }
+        }
+    }
+}
+
+@Composable
+private fun ChartLoading(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Loading plot...")
+    }
+}
+
+@Composable
+private fun Chart(
+    modelProducer: CartesianChartModelProducer,
+    workoutSets: List<SeriesValue>,
+    modifier: Modifier = Modifier
+) {
+    val baseLineComponent = rememberLineComponent(
+        strokeThickness = 0.dp,
+        strokeFill = Fill(Color.Black.toArgb())
+    )
+    ProvideVicoTheme(rememberM3VicoTheme()) {
+        CartesianChartHost(
+            modifier = modifier,
+            modelProducer = modelProducer,
+            chart = rememberCartesianChart(
+                rememberColumnCartesianLayer(
+                    columnProvider = remember(baseLineComponent) {
+                        columnProvider(baseLineComponent)
+                    },
+                    columnCollectionSpacing = 0.dp,
+                ),
+                startAxis = VerticalAxis.rememberStart(),
+                bottomAxis = bottomAxis(workoutSets)
+            ),
+        )
+    }
+}
+
+@Composable
+private fun bottomAxis(workoutSets: List<SeriesValue>): HorizontalAxis<Axis.Position.Horizontal.Bottom> =
+    HorizontalAxis.rememberBottom(
+        valueFormatter = { _, value, _ ->
+            workoutSets.getOrNull(value.toInt())?.label ?: "-"
+        }
+    )
+
+private fun columnProvider(baseLineComponent: LineComponent): ColumnCartesianLayer.ColumnProvider =
+    object : ColumnCartesianLayer.ColumnProvider {
+        override fun getColumn(
+            entry: ColumnCartesianLayerModel.Entry,
+            seriesIndex: Int,
+            extraStore: ExtraStore
+        ): LineComponent {
+            val colors = extraStore[difficultyColorsKey]
+            val targetColor = colors.getOrNull(entry.x.toInt())
+                ?: Fill(Color.Black.toArgb())
+            return baseLineComponent.copy(
+                fill = targetColor,
+            )
+        }
+
+        override fun getWidestSeriesColumn(
+            seriesIndex: Int,
+            extraStore: ExtraStore
+        ): LineComponent {
+            return baseLineComponent
+        }
+    }
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 private fun preview(){
     val start = LocalDateTime.now()
     val green = Fill(Color.Green.toArgb())
-    val yellow = Fill(Color.Yellow.toArgb())
+    val yellow = Fill(Color(0xFFFF9800).toArgb())
     val red = Fill(Color.Red.toArgb())
     val format = DateTimeFormatter.ofPattern("MM/dd")
-    BarPlot(
-        (0..20).flatMap { i ->
-            val day = start.plusWeeks(i.toLong())
-            listOf(
-                SetRecord(day.plusMinutes(0).format(format), (i*5) + 10f, green),
-                SetRecord(day.plusMinutes(5).format(format), (i*5) + 10f, green),
-                SetRecord(day.plusMinutes(10).format(format), (i*5) + 10f, listOf(yellow, green).random()),
-                SetRecord(day.plusDays(1).format(format), 0f, green),
-                SetRecord(day.plusDays(2).format(format), 0f, green),
-                SetRecord(day.plusDays(3).format(format), 0f, green),
-                SetRecord(day.plusDays(4).plusMinutes(0).format(format), (i*5) + 10f, green),
-                SetRecord(day.plusDays(4).plusMinutes(5).format(format), (i*5) + 10f, green),
-                SetRecord(day.plusDays(4).plusMinutes(10).format(format), (i*5) + 10f, listOf(yellow, green).random()),
-                SetRecord(day.plusDays(5).format(format), 0f, green),
-                SetRecord(day.plusDays(6).format(format), 0f, green),
-                SetRecord(day.plusDays(7).format(format), 0f, green),
-            ).take(100)
-        }
-    )
+    StrongGiraffeTheme() {
+        BarPlot(
+            (0..20).flatMap { i ->
+                val day = start.plusWeeks(i.toLong())
+                listOf(
+                    SeriesValue(day.plusMinutes(0).format(format), (i*5) + 10f, green),
+                    SeriesValue(day.plusMinutes(5).format(format), (i*5) + 10f, green),
+                    SeriesValue(day.plusMinutes(10).format(format), (i*5) + 10f, listOf(yellow, green).random()),
+                    SeriesValue(day.plusDays(1).format(format), 0f, green),
+                    SeriesValue(day.plusDays(2).format(format), 0f, green),
+                    SeriesValue(day.plusDays(3).format(format), 0f, green),
+                    SeriesValue(day.plusDays(4).plusMinutes(0).format(format), (i*5) + 10f, green),
+                    SeriesValue(day.plusDays(4).plusMinutes(5).format(format), (i*5) + 10f, green),
+                    SeriesValue(day.plusDays(4).plusMinutes(10).format(format), (i*5) + 10f, listOf(yellow, green, red).random()),
+                    SeriesValue(day.plusDays(5).format(format), 0f, green),
+                    SeriesValue(day.plusDays(6).format(format), 0f, green),
+                    SeriesValue(day.plusDays(7).format(format), 0f, green),
+                )
+            }.take(50),
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(0.5f)
+        )
+    }
 }
