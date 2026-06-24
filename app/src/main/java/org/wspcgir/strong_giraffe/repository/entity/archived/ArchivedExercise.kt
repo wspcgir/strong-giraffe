646492885b0bce0e882ab06c0156c86a0d355052
JD95 <jeffreydwyer95@outlook.com>
2026-06-23 22:51:11 -0700

    Defines archived exercise table

---
+++ app/src/main/java/org/wspcgir/strong_giraffe/repository/entity/archived/ArchivedExercise.kt
@@ -0,0 +1,27 @@
+package org.wspcgir.strong_giraffe.repository.entity.archived
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import org.wspcgir.strong_giraffe.repository.entity.Exercise
+
+@Entity(
+    indices = [
+        Index(value = arrayOf("id"))
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = Exercise::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("exercise"),
+            onDelete = CASCADE
+        )
+    ]
+)
+data class ArchivedExercise (
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "exercise") val exercise: String
+)
